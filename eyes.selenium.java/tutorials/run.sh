74f17d11c070f8e5535e213b09b634a186b1db7a
artem <artem.borodavka@applitools.com>
2020-09-24 16:41:45 +0300

    update docker file to contain all the build steps from the base image of the ubuntu add tutorials checks for the java3 SDK

---
+++ eyes.selenium.java/tutorials/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+$DIR/build.sh
+echo run.sh
+docker run -e APPLITOOLS_API_KEY tutorial_java_basic
+docker run -e APPLITOOLS_API_KEY tutorial_java_ufg
+$DIR/report.sh
