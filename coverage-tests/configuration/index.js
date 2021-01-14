9d5d45e4d1c84889d5f6a7a558605bd395534641
Artem-applitools <59561599+Artem-applitools@users.noreply.github.com>
2021-01-14 13:22:13 +0200

    update java coverage tests to use implementation from sdk.coverage.tests branch (#453)
    update README for coverage tests

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,12 +1,9 @@
-const overrideTests = require('./override-tests')
-const initializeSdk = require('./initialize')
-const testFrameworkTemplate = require('./template')
 
 module.exports = {
   name: 'eyes_selenium_java',
-  initializeSdk: initializeSdk,
-  overrideTests,
-  testFrameworkTemplate: testFrameworkTemplate,
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/emitter.js',
+  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/overrides.js',
+  template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/template.hbs',
   ext: '.java',
   outPath: './src/test/java/coverage/generic',
 }
