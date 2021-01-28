643b25f9871f77eb5a4e60d26726470e2265435e
artem <artem.borodavka@applitools.com>
2021-01-28 15:22:49 +0200

    add mvn clean install to travis.yml add xvfb add chromedriver download

--- eyes.selenium.java/tutorials/build.sh
+++ eyes.selenium.java/tutorials/build.sh
@@ -2,15 +2,11 @@
 
 set -e
 DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd $DIR/../
-mvn clean package -Dmaven.test.skip=true
-rm ./tutorials/base/eyes-selenium* || true
-mv ./target/eyes-selenium-java*jar-with-dependencies.jar ./tutorials/base/
-regex='eyes-selenium-java3-([0-9].[0-9]+.[0-9]+)'
-[[ $(ls tutorials/base | grep eyes-selenium) =~ $regex ]]
-version="${BASH_REMATCH[1]}"
-echo Current package version is $version
-cd $DIR
-docker build --build-arg version=$version -t tutorial_java -f ./base/Dockerfile .
-docker build --no-cache -t tutorial_java_basic -f ./basic/Dockerfile .
-docker build --no-cache -t tutorial_java_ufg -f ./ultrafastgrid/Dockerfile .
+
+mkdir $DIR/tmp
+cd $DIR/tmp
+git clone https://github.com/applitools/tutorial-selenium-java-basic.git
+sed -i 's/"APPLITOOLS_API_KEY"/System.getenv("APPLITOOLS_API_KEY")/g' tutorial-selenium-java-basic/src/test/java/com/applitools/quickstarts/BasicDemo.java
+git clone https://github.com/applitools/tutorial-selenium-java-ultrafastgrid.git
+sed -i 's/"APPLITOOLS_API_KEY"/System.getenv("APPLITOOLS_API_KEY")/g' tutorial-selenium-java-ultrafastgrid/src/test/java/com/applitools/quickstarts/AppTest.java
+sed -i 's/new VisualGridRunner(1)/new VisualGridRunner(7)/g' tutorial-selenium-java-ultrafastgrid/src/test/java/com/applitools/quickstarts/AppTest.java
