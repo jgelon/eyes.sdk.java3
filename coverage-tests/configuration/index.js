65d4e1575159f768489e5007786a6646efecb813
YuriyCheTest <yurii.cherepovskiy@applitools.com>
2021-03-07 23:32:32 +0200

    Local overrides - remove skip for 42 generic tests

--- coverage-tests/configuration/index.js
+++ coverage-tests/configuration/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: 'eyes_selenium_java',
   emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/emitter.js',
-  overrides: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/overrides.js',
+  overrides: '../../../../../overrides.js',
   template: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/java/template.hbs',
   ext: '.java',
   outPath: './src/test/java/coverage/generic',
---
+++ coverage-tests/overrides.js
@@ -0,0 +1,31 @@
+module.exports = {
+    // JS specific, no need to implement in java
+    'should return test results from close with passed classic test': {skipEmit: true}, // skipped
+    'should return test results from close with passed vg test': {skipEmit: true}, //   cause
+    'should return test results from close with failed classic test': {skipEmit: true}, // tests
+    'should return test results from close with failed vg test': {skipEmit: true}, // JS specific
+    'should handle check of stale element in frame if selector is preserved': {skipEmit: true}, // Not implemented yet
+
+    'should send dom and location when check window': {skipEmit: true},
+    'should send dom and location when check window with vg': {skipEmit: true},
+    'should send dom and location when check window fully': {skipEmit: true},
+    'should send dom and location when check window fully with vg': {skipEmit: true},
+    'should send dom and location when check frame': {skipEmit: true},
+    'should send dom and location when check frame with vg': {skipEmit: true},
+    'should send dom and location when check frame fully': {skipEmit: true},
+    'should send dom and location when check frame fully with vg': {skip: true}, // not supported by ufg
+    'should send dom and location when check region by selector': {skipEmit: true},
+    'should send dom and location when check region by selector with vg': {skipEmit: true},
+    'should send dom and location when check region by selector fully': {skipEmit: true},
+    'should send dom and location when check region by selector fully with vg': {skipEmit: true},
+    'should send dom and location when check region by selector in frame': {skipEmit: true},
+    'should send dom and location when check region by selector with custom scroll root': {skipEmit: true},
+    'should send dom and location when check region by selector with custom scroll root with vg': {skipEmit: true},
+    'should send dom and location when check region by selector fully with custom scroll root': {skipEmit: true},
+    'should send dom and location when check region by selector fully with custom scroll root with vg': {skipEmit: true},
+    'should send dom of version 11': {skipEmit: true},
+    'should not fail if scroll root is stale on android': {skipEmit: true},
+    'check region by selector in frame fully on firefox legacy': { skipEmit: true },
+    'adopted styleSheets on chrome': {skipEmit: true},
+	'adopted styleSheets on firefox': {skipEmit: true},
+}
